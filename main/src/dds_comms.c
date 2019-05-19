4c9205a35ef9d7aa2b35202bfaf1cb91bc1517e7
Ian Wakely <raveious.irw@gmail.com>
2019-05-19 07:15:28 -0400

    Cleaning up the discovery code.

--- main/src/dds_comms.c
+++ main/src/dds_comms.c
@@ -11,33 +11,51 @@ bool on_agent_found(const uxrAgentAddress* address, int64_t timestamp, void* arg
     return false;
 }
 
+bool discover_agent(uxrAgentAddress* chosen_agent) {
+    #if CONFIG_UXR_DISCOVERY_TYPE_UNICAST
+        /// TODO: Add support to list multiple IP addresses
+        size_t agent_list_size = 1;
+        uxrAgentAddress agent_list[agent_list_size];
+
+        /// TODO: Might not want to rebuild this list every time
+        strncpy(agent_list[agent_list_size].ip,
+                CONFIG_UXR_DISCOVERY_IP,
+                sizeof(agent_list[agent_list_size].ip));
+
+        agent_list[agent_list_size].port = CONFIG_UXR_DISCOVERY_PORT;
+
+        return uxr_discovery_agents_unicast(  CONFIG_UXR_DISCOVERY_ATTEMPTS,
+                                              CONFIG_UXR_DISCOVERY_PERIOD,
+                                              on_agent_found,
+                                              NULL,
+                                              chosen_agent,
+                                              agent_list,
+                                              agent_list_size);
+
+    #else
+        return uxr_discovery_agents_multicast(CONFIG_UXR_DISCOVERY_ATTEMPTS,
+                                              CONFIG_UXR_DISCOVERY_PERIOD,
+                                              on_agent_found,
+                                              NULL,
+                                              chosen_agent);
+    #endif
+}
+
 void dds_task(void) {
 
     uxrAgentAddress chosen;
 
-    #if CONFIG_UXR_DISCOVERY_TYPE_UNICAST
-    // Eventually need to add support to list multiple IP addresses
-    size_t agent_list_size = 1;
-    uxrAgentAddress agent_list[1];
-    strcpy(agent_list[agent_list_size].ip, CONFIG_UXR_DISCOVERY_IP);
-    agent_list[agent_list_size].port = CONFIG_UXR_DISCOVERY_PORT;
-    #endif
-
     while (true) {
-        #if CONFIG_UXR_DISCOVERY_TYPE_UNICAST
-        if(uxr_discovery_agents_multicast(CONFIG_UXR_DISCOVERY_ATTEMPTS, CONFIG_UXR_DISCOVERY_PERIOD, on_agent_found, NULL, &chosen)) {
-        #else
-        if(uxr_discovery_agents_unicast(CONFIG_UXR_DISCOVERY_ATTEMPTS, CONFIG_UXR_DISCOVERY_PERIOD, on_agent_found, NULL, &chosen, agent_list, agent_list_size)) {
-        #endif
+        if (discover_agent(&chosen)) {
             // True -> The user returns true in the callback.
             printf("Chosen agent => ip: %s, port: %d\n", chosen.ip, chosen.port);
             break;
         } else {
             printf("Failed to discover an agent @ %lli.\n", esp_timer_get_time());
             fflush(stdout);
-       
+
             vTaskDelay(1000 / portTICK_PERIOD_MS);
-       
+
             //esp_restart();
         }
     }
