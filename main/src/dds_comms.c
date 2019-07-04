9543d4cd327da3a9be37d84f0e9aecdd58a41c1f
Ian Wakely <raveious.irw@gmail.com>
2019-07-04 12:08:26 -0400

    Working session creation but discovery API changed

--- main/src/dds_comms.c
+++ main/src/dds_comms.c
@@ -4,10 +4,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <esp_wifi.h>
 
 // Handle to save for the dds task
-TaskHandle_t task_handle = NULL;
+TaskHandle_t dds_data_task = NULL;
+// TaskHandle_t agend_discovery_task = NULL;
 
+// Selected agent to use
+uxrAgentAddress chosen_agent = { CONFIG_UXR_DISCOVERY_IP, CONFIG_UXR_DISCOVERY_PORT };
+
+// Session that was created
+uxrSession session;
+
+// Transport information
 #ifdef CONFIG_PROFILE_UDP_TRANSPORT
 uxrUDPTransport transport;
 uxrUDPPlatform platform;
@@ -18,48 +27,92 @@ uxrTCPPlatform platform;
 #error No transport was selected.
 #endif
 
-bool on_agent_found(const uxrAgentAddress* address, int64_t timestamp, void* args) {
-    (void) timestamp; (void) args;
+// void on_agent_found(const uxrAgentAddress* address, void* args) {
+//     (void) args;
 
-    printf("Found agent => ip: %s, port: %d\n", address->ip, address->port);
+//     printf("Found agent => ip: %s, port: %d\n", address->ip, address->port);
+// }
 
-    return false;
-}
+// void discover_agent_task(void* found_agent_func) {
+//     #if CONFIG_UXR_DISCOVERY_TYPE_UNICAST
+//         /// TODO: Add support to list multiple IP addresses
+//         size_t agent_list_size = 1;
+//         uxrAgentAddress agent_list[agent_list_size];
 
-/*
-bool discover_agent(uxrAgentAddress* agent, void* found_agent_func) {
-    #if CONFIG_UXR_DISCOVERY_TYPE_UNICAST
-        /// TODO: Add support to list multiple IP addresses
-        size_t agent_list_size = 1;
-        uxrAgentAddress agent_list[agent_list_size];
+//         agent_list[agent_list_size].ip   = CONFIG_UXR_DISCOVERY_IP;
+//         agent_list[agent_list_size].port = CONFIG_UXR_DISCOVERY_PORT;
 
-        /// TODO: Might not want to rebuild this list every time
-        strncpy(agent_list[agent_list_size].ip,
-                CONFIG_UXR_DISCOVERY_IP,
-                sizeof(agent_list[agent_list_size].ip));
+//         uxr_discovery_agents( CONFIG_UXR_DISCOVERY_ATTEMPTS,
+//                                      CONFIG_UXR_DISCOVERY_PERIOD,
+//                                      found_agent_func,
+//                                      NULL,
+//                                      agent_list,
+//                                      agent_list_size
+//                                     );
 
-        agent_list[agent_list_size].port = CONFIG_UXR_DISCOVERY_PORT;
-
-        return uxr_discovery_agents_unicast(  CONFIG_UXR_DISCOVERY_ATTEMPTS,
-                                              CONFIG_UXR_DISCOVERY_PERIOD,
-                                              found_agent_func,
-                                              NULL,
-                                              agent,
-                                              agent_list,
-                                              agent_list_size);
-
-    #else
-        return uxr_discovery_agents_multicast(CONFIG_UXR_DISCOVERY_ATTEMPTS,
-                                              CONFIG_UXR_DISCOVERY_PERIOD,
-                                              found_agent_func,
-                                              NULL,
-                                              agent);
-    #endif
-}
-*/
+//     #else
+//         uxr_discovery_agents_default( CONFIG_UXR_DISCOVERY_ATTEMPTS,
+//                                              CONFIG_UXR_DISCOVERY_PERIOD,
+//                                              found_agent_func,
+//                                              NULL
+//                                             );
+//     #endif
+// }
 
 void dds_task(void* parms) {
     (void) parms;
+
+    // Discover the agent that will be used for later
+    // while (true) {
+    //     if (discover_agent(&chosen_agent, &on_agent_found)) {
+    //         // True -> The user returns true in the callback.
+    //         printf("Chosen agent => ip: %s, port: %d\n", chosen_agent.ip, chosen_agent.port);
+    //         break;
+    //     } else {
+    //         printf("Failed to discover an agent @ %lli.\n", esp_timer_get_time());
+    //         fflush(stdout);
+
+    //         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+    //         //esp_restart();
+    //     }
+    // }
+
+    // Setup the transport
+    #ifdef CONFIG_PROFILE_UDP_TRANSPORT
+    if (!uxr_init_udp_transport(&transport, &platform, chosen_agent.ip, chosen_agent.port)) {
+        printf("Failed to create transport.");
+        // return;
+    }
+    #else
+    if (!uxr_init_tcp_transport(&transport, &platform, chosen_agent.ip, chosen_agent.port)) {
+        printf("Failed to create transport.");
+        // return;
+    }
+    #endif
+
+    uint8_t mac_addr[6];
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, mac_addr);
+
+    uint32_t session_id = 0;
+    session_id |= mac_addr[2];
+    session_id <<= 8;
+    session_id |= mac_addr[3];
+    session_id <<= 8;
+    session_id |= mac_addr[4];
+    session_id <<= 8;
+    session_id |= mac_addr[5];
+
+    uxr_init_session(&session, &transport.comm, session_id);
+    if (!uxr_create_session(&session))
+    {
+        printf("Failed to create session with agent.\n");
+        printf("ID: %hhu\n", session.info.id);
+        printf("Key: 0x%02hhX 0x%02hhX 0x%02hhX 0x%02hhX\n", session.info.key[0], session.info.key[1], session.info.key[2], session.info.key[3]);
+        printf("Requested Status: %hu\n", session.info.last_requested_status);
+        printf("Request ID: %hhu\n", session.info.last_request_id);
+        // return;
+    }
 
     printf("Node online!");
     fflush(stdout);
@@ -70,71 +123,22 @@ void dds_task(void* parms) {
 }
 
 void start_dds_task(void) {
-    BaseType_t task_status;
-
-    if (task_handle == NULL) {
-
-        // Selected agent to use
-        /*
-        uxrAgentAddress chosen_agent;
-
-        // Discover the agent that will be used for later
-        while (true) {
-            if (discover_agent(&chosen_agent, &on_agent_found)) {
-                // True -> The user returns true in the callback.
-                printf("Chosen agent => ip: %s, port: %d\n", chosen.ip, chosen.port);
-                break;
-            } else {
-                printf("Failed to discover an agent @ %lli.\n", esp_timer_get_time());
-                fflush(stdout);
-
-                vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-                //esp_restart();
-            }
-        }
-        */
-
-        #ifdef CONFIG_PROFILE_UDP_TRANSPORT
-        if (!uxr_init_udp_transport(&transport, &platform, "192.168.10.150", 20000)) {
-            printf("Failed to create transport.");
-            // return;
-        }
-        #else
-        if (!uxr_init_tcp_transport(&transport, &platform, "192.168.10.150", 20000)) {
-            printf("Failed to create transport.");
-            // return;
-        }
-        #endif
-
-        uxrSession session;
-        uxr_init_session(&session, &transport.comm, 0x00112233);
-        if (!uxr_create_session(&session))
-        {
-            printf("Failed to create session with agent.\n");
-            printf("ID: %hhu\n", session.info.id);
-            printf("Key: 0x%02hhX 0x%02hhX 0x%02hhX 0x%02hhX\n", session.info.key[0], session.info.key[1], session.info.key[2], session.info.key[3]);
-            printf("Requested Status: %hu\n", session.info.last_requested_status);
-            printf("Request ID: %hhu\n", session.info.last_request_id);
-            // return;
-        }
-
+    if (dds_data_task == NULL) {
         // Create the task to run DDS out of.
-        task_status = xTaskCreatePinnedToCore(dds_task,
-                                            "DDS_TASK",
-                                            4096, //STACK_SIZE,
-                                            NULL,
-                                            configMAX_PRIORITIES - 1,
-                                            &task_handle,
-                                            portNUM_PROCESSORS - 1
-                                            );
+        xTaskCreate(dds_task,
+                    "DDS_TASK",
+                    (8 * 1024), //STACK_SIZE,
+                    NULL,
+                    configMAX_PRIORITIES - 1,
+                    &dds_data_task
+                    );
     } else {
-        vTaskResume(task_handle);
+        vTaskResume(dds_data_task);
     }
 }
 
 void stop_dds_task(void) {
-    if (task_handle != NULL) {
-        vTaskSuspend(task_handle);
+    if (dds_data_task != NULL) {
+        vTaskSuspend(dds_data_task);
     }
 }
