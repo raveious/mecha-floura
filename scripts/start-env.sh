dc499b738b014b403170b0d49e2eacbf2a26ea16
Ian Wakely <raveious.irw@gmail.com>
2019-07-17 21:07:48 -0400

    Adding the ability to mount multiple devices into the docker container.

--- scripts/start-env.sh
+++ scripts/start-env.sh
@@ -8,9 +8,11 @@ if [ -z "$(docker image ls | grep $repo_name)" ]; then
     docker build -t $repo_name .
 fi
 
-if [ -c "$1" ]; then
-    echo "Mounting $1 into the container..."
-    device_arg="--device $1"
-fi
+for dev in "$@"; do
+    if [ -c "$dev" ]; then
+        echo "Mounting $dev into the container..."
+        device_arg="$device_arg --device $dev"
+    fi
+done
 
 docker run -it --rm -u $(id -u $UID):$(id -g $UID) $device_arg -v $repo_dir:/$repo_name $repo_name
